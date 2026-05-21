cf0db0317b77a9d45b32f4116248094e01cf5799
jayashreeseereon <jayashree.seereon@gmail.com>
2026-05-21 13:13:53 +0530

    update

---
+++ src/api/subcategory.jsx
@@ -0,0 +1,21 @@
+import api from "./axios";
+
+export const getSubCategories = async () => {
+  const res = await api.get("/api/subcategories/");
+  return res.data;
+};
+
+export const createSubCategory = async (payload, config = {}) => {
+  const res = await api.post("/api/subcategories/", payload, config);
+  return res.data;
+};
+
+export const updateSubCategory = async (id, payload, config = {}) => {
+  const res = await api.put(`/api/subcategories/${id}`, payload, config);
+  return res.data;
+};
+
+export const deleteSubCategory = async (id) => {
+  const res = await api.delete(`/api/subcategories/${id}`);
+  return res.data;
+};
